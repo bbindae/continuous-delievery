319f0c4f9b8d7370b07831d445933298b40b4bdc
hkim7 <hkim@lampsplus.com>
2015-06-08 18:32:22 -0700

    Adding Package

---
+++ ContinuousDelivery.Tests/SmokeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ContinuousDelivery.Tests
+{
+    [TestFixture]
+    public class SmokeTests
+    {
+        [Test]
+        public void Can_Add_Two_Numbers()
+        {
+            var num1 = 1;
+            var num2 = 2;
+
+            var num3 = num1 + num2;
+
+            Assert.AreEqual(3, num3);
+
+        }
+    }
+}
